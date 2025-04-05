1116b35f27e334a16e17afa2370153f18a6f4c98
vadim <vadym.pypko@betterme.world>
2025-04-05 21:20:10 +0300

    HomeRU discord

---
+++ HomeRU.tsx
@@ -0,0 +1,11 @@
+const HomeRU = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Пластический хирург</title>
+        <html lang="ru" />
+      </Helmet>
+      <h1>Киев</h1>
+    </>
+  );
+};
