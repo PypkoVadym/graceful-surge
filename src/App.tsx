c7d8bc24d15bb936b190ef70689e439c3e94a789
vadim <vadym.pypko@betterme.world>
2025-04-05 21:49:20 +0300

    imported helmet

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,21 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HelmetProvider } from 'react-helmet-async';
 import Index from "./pages/Index";
 import HomeRU from './pages/HomeRU';
 import NotFound from "./pages/NotFound";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
 import TermsOfUse from "./pages/TermsOfUse";
 import ThankYou from "./pages/ThankYou";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <HelmetProvider>
+    <App />
+  </HelmetProvider>,
+  document.getElementById('root')
+);
 
 const queryClient = new QueryClient();
 
