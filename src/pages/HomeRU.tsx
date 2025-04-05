7822c573e53ebbc9226dfd161017e488d7d515b6
vadim <vadym.pypko@betterme.world>
2025-04-05 21:51:22 +0300

    Once again

--- src/pages/HomeRU.tsx
+++ src/pages/HomeRU.tsx
@@ -1,30 +1,15 @@
 
-import React from 'react';
-import Header from '../components/russian/HeaderRu';
-import Footer from '../components/russian/FooterRu';
-import HeroSectionRu from '../components/russian/HeroSectionRu';
-import AboutSectionRu from '../components/russian/AboutSectionRu';
-import ServicesSectionRu from '../components/russian/ServicesSectionRu';
-import TestimonialsSectionRu from '../components/russian/TestimonialsSectionRu';
-import ClinicSectionRu from '../components/russian/ClinicSectionRu';
-import ContactSectionRu from '../components/russian/ContactSectionRu';
-import SEOHead from '../components/SEOHead';
+
 import { Helmet } from 'react-helmet-async';
 
 const HomeRU = () => {
   return (
     <>
-      <SEOHead isRussianVersion={true} />
-      <Header />
-      <main>
-        <HeroSectionRu />
-        <AboutSectionRu />
-        <ServicesSectionRu />
-        <TestimonialsSectionRu />
-        <ClinicSectionRu />
-        <ContactSectionRu />
-      </main>
-      <Footer />
+      <Helmet>
+        <title>ЫЫЫЫ</title>
+        <html lang="ru" />
+      </Helmet>
+      <h1>ыыыыы</h1>
     </>
   );
 };
