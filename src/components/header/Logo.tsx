beea66ef00cfa9f5fec9e2e06c849185ea8601d5
vadim <vadym.pypko@betterme.world>
2025-04-29 19:11:58 +0300

    deleted onClick in logo

--- src/components/header/Logo.tsx
+++ src/components/header/Logo.tsx
@@ -8,9 +8,7 @@ interface LogoProps {
 const Logo: React.FC<LogoProps> = ({ onClick }) => {
   return (
     <div className="flex items-center">
-      <a className="text-2xl font-display font-semibold tracking-tight"
-        onClick={onClick}
-      >
+      <a className="text-2xl font-display font-semibold tracking-tight">
         <span className="text-primary">Денис</span>Маркович
       </a>
     </div>
--- src/components/russian/LogoRu.tsx
+++ src/components/russian/LogoRu.tsx
@@ -8,9 +8,7 @@ interface LogoRuProps {
 const LogoRu: React.FC<LogoRuProps> = ({ onClick }) => {
   return (
     <div className="flex items-center">
-      <a className="text-2xl font-display font-semibold tracking-tight"
-        onClick={onClick}
-      >
+      <a className="text-2xl font-display font-semibold tracking-tight">
         <span className="text-primary">Денис</span>Маркович
       </a>
     </div>
